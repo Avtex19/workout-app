af9a4abcf0b417307fc65d204faaba6ef4635ca6
Avtex19 <114765308+Avtex19@users.noreply.github.com>
2024-03-13 15:31:21 +0400

    feat: created models and added initial data to database

--- back/myapp/admin.py
+++ back/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Workout)
+admin.site.register(Workout_category)
+admin.site.register(UserProfile)
+
--- back/myapp/models.py
+++ back/myapp/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
 
-# Create your models here.
+
+class Workout_category(models.Model):
+    workout_type_id = models.IntegerField(primary_key=True)
+    workout_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.workout_name
+
+
+class Workout(models.Model):
+    workout_name = models.CharField(max_length=50)
+    workout_sets_n = models.IntegerField()
+    workout_set_duration_m = models.IntegerField()
+    workout_category = models.ForeignKey(Workout_category, on_delete=models.CASCADE)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.workout_name
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    height = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    weight = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    password = models.TextField(max_length=255, null=False, blank=False, default="default")
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def populate_personal_info(sender, instance, created, **kwargs):
+    if created:
+        instance.first_name = "Default First Name"
+        instance.last_name = "Default Last Name"
+        instance.save()
